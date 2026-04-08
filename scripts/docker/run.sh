818e8ce17351b61fcfc49871cf5efe7ce34f03a7
Ilan Iwumbwe <ikibfm@gmail.com>
2026-04-08 22:50:40 +0100

    fix docker run script

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -7,5 +7,5 @@ IMAGE_NAME="qutefuzz:latest"
 IMAGE_NAME="qutefuzz:latest"
 
 docker run -it --rm -v "$(pwd):/app" $IMAGE_NAME \
-    bash -c "uv sync && uv run scripts/run.py $@"
+    bash -c 'uv sync && uv run scripts/run.py "$@"'
 
