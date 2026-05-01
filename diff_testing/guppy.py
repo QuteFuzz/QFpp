d1de73560be084ed6e353e62a10ab3f600b93821
Ilan Iwumbwe <ikibfm@gmail.com>
2026-05-01 18:12:08 +0100

    ignore typing for callable return in guppy script

--- diff_testing/guppy.py
+++ diff_testing/guppy.py
@@ -51,8 +51,9 @@ def _n_qubits_from_hugr(hugr) -> int:
     for attr in ("num_qubits", "n_qubits", "qubit_count"):
         try:
             val = getattr(hugr, attr)
-            return int(val() if callable(val) else val)
-        except (AttributeError, TypeError):
+            result = val() if callable(val) else val        
+            return int(result)  #type: ignore
+        except (AttributeError, TypeError, ValueError):
             pass
     return 0
 
