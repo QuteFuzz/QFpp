cb4948a56f21be308699dcce18040118fe0c94ea
Ilan Iwumbwe <ikibfm@gmail.com>
2026-05-26 16:26:30 +0100

    only route at opt >= 1

--- diff_testing/qiskit.py
+++ diff_testing/qiskit.py
@@ -39,7 +39,7 @@ class qiskitTesting(Base):
 
         cmap = self.topo_maker(circuit.num_qubits) if circuit.num_qubits >= 2 else None
 
-        if cmap is not None:
+        if cmap is not None and opt_level >= 1:
             basis_gates = ["cx", "id", "rz", "sx", "x", "y", "z", "h", "s", "t"]
             circ_prime = transpile(
                 circuit, basis_gates=basis_gates, coupling_map=cmap, optimization_level=opt_level
